beaf419d8ebe71fee6d020ef8f0655d56108968e
SHOOTER-MAKER <82651621+SHOOTER-MAKER@users.noreply.github.com>
2024-06-21 10:59:21 +0500

    Update run.py

--- run.py
+++ run.py
@@ -2,28 +2,24 @@
 import os, sys, platform
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf clone.so clone32.so')
+        os.system('rm -rf clone*')
 except:
     pass
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('clone.so'):
-        os.system('curl -L https://github.com/SHOOTER-MAKER/Juttbrand/blob/main/clone.cpython-311.so?raw=true -o clone.so')
-        os.system('chmod 777 clone.so')
-        from clone import reg
-        reg()
+    if not os.path.isfile('clone'):
+        os.system('curl -L https://github.com/SHOOTER-MAKER/Juttbrand/blob/main/clone?raw=true -o clone')
+        os.system('chmod 777 clone')
+        os.system('./clone')
     else:
-        from clone import reg
-        reg()
+        os.system('./clone')
 elif bit == '32bit':
-    if not os.path.isfile('clone32.so'):
-        os.system('curl -L https://github.com/SHOOTER-MAKER/Juttbrand/blob/main/clone32.cpython-311.so?raw=true -o clone32.so')
-        os.system('chmod 777 clone32.so')
-        from clone32 import reg
-        reg()
+    if not os.path.isfile('clone32'):
+        os.system('curl -L https://github.com/SHOOTER-MAKER/Juttbrand/blob/main/clone32?raw=true -o clone32')
+        os.system('chmod 777 clone32')
+        os.system('./clone32')
     else:
-        from clone32 import reg
-        reg()
+        os.system('./clone32')
 else:
     print ('Your device is not supported ')
     exit()
